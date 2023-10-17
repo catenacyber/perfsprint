27abfc8246b33c5933951760bfc01de27d7802f1
Anton Telyshev <anton.telishev@yandex.ru>
2023-10-17 17:25:02 +0300

    tdd: much more tests

--- analyzer/analyzer_test.go
+++ analyzer/analyzer_test.go
@@ -1,8 +1,10 @@
 package analyzer_test
 
 import (
-	"os"
-	"path/filepath"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"strconv"
 	"testing"
 
 	"github.com/catenacyber/gostrconv/analyzer"
@@ -10,11 +12,56 @@ import (
 )
 
 func TestAll(t *testing.T) {
-	wd, err := os.Getwd()
-	if err != nil {
-		t.Fatalf("Failed to get wd: %s", err)
-	}
+	t.Parallel()
+	analysistest.RunWithSuggestedFixes(t, analysistest.TestData(), analyzer.Analyzer, "p")
+}
 
-	testdata := filepath.Join(filepath.Dir(wd), "testdata")
-	analysistest.Run(t, testdata, analyzer.Analyzer, "p")
+func TestReplacements(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		before, after string
+	}{
+		{before: fmt.Sprintf("%s", "hello"), after: "hello"},
+		{before: fmt.Sprintf("%v", "hello"), after: "hello"},
+
+		{before: fmt.Sprintf("%s", io.EOF), after: io.EOF.Error()},
+		{before: fmt.Sprintf("%v", io.EOF), after: io.EOF.Error()},
+
+		{before: fmt.Sprintf("%t", true), after: strconv.FormatBool(true)},
+		{before: fmt.Sprintf("%v", true), after: strconv.FormatBool(true)},
+		{before: fmt.Sprintf("%t", false), after: strconv.FormatBool(false)},
+		{before: fmt.Sprintf("%v", false), after: strconv.FormatBool(false)},
+
+		{before: fmt.Sprintf("%x", []byte{'a', 'b', 'c'}), after: hex.EncodeToString([]byte{'a', 'b', 'c'})},
+
+		{before: fmt.Sprintf("%d", 42), after: strconv.Itoa(42)},
+		{before: fmt.Sprintf("%v", 42), after: strconv.Itoa(42)},
+		{before: fmt.Sprintf("%d", int8(42)), after: strconv.Itoa(int(int8(42)))},
+		{before: fmt.Sprintf("%v", int8(42)), after: strconv.Itoa(int(int8(42)))},
+		{before: fmt.Sprintf("%d", int16(42)), after: strconv.Itoa(int(int16(42)))},
+		{before: fmt.Sprintf("%v", int16(42)), after: strconv.Itoa(int(int16(42)))},
+		{before: fmt.Sprintf("%d", int32(42)), after: strconv.Itoa(int(int32(42)))},
+		{before: fmt.Sprintf("%v", int32(42)), after: strconv.Itoa(int(int32(42)))},
+		{before: fmt.Sprintf("%d", int64(42)), after: strconv.FormatInt(int64(42), 10)},
+		{before: fmt.Sprintf("%v", int64(42)), after: strconv.FormatInt(int64(42), 10)},
+
+		{before: fmt.Sprintf("%d", uint(42)), after: strconv.FormatUint(uint64(uint(42)), 10)},
+		{before: fmt.Sprintf("%v", uint(42)), after: strconv.FormatUint(uint64(uint(42)), 10)},
+		{before: fmt.Sprintf("%d", uint8(42)), after: strconv.FormatUint(uint64(uint8(42)), 10)},
+		{before: fmt.Sprintf("%v", uint8(42)), after: strconv.FormatUint(uint64(uint8(42)), 10)},
+		{before: fmt.Sprintf("%d", uint16(42)), after: strconv.FormatUint(uint64(uint16(42)), 10)},
+		{before: fmt.Sprintf("%v", uint16(42)), after: strconv.FormatUint(uint64(uint16(42)), 10)},
+		{before: fmt.Sprintf("%d", uint32(42)), after: strconv.FormatUint(uint64(uint32(42)), 10)},
+		{before: fmt.Sprintf("%v", uint32(42)), after: strconv.FormatUint(uint64(uint32(42)), 10)},
+		{before: fmt.Sprintf("%d", uint64(42)), after: strconv.FormatUint(uint64(42), 10)},
+		{before: fmt.Sprintf("%v", uint64(42)), after: strconv.FormatUint(uint64(42), 10)},
+	}
+	for _, tt := range cases {
+		t.Run("", func(t *testing.T) {
+			if tt.before != tt.after {
+				t.Fatalf("%s != %s", tt.before, tt.after)
+			}
+		})
+	}
 }
