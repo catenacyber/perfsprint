94b53d906abe32e514fc4bc3e8b1fd5e6542266c
Philippe Antoine <contact@catenacyber.fr>
2024-02-02 21:59:09 +0100

    analyzer: make imports fixing optional

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -17,18 +17,20 @@ import (
 )
 
 type perfSprint struct {
-	intConv  bool
-	errError bool
-	errorf   bool
-	sprintf1 bool
+	intConv    bool
+	errError   bool
+	errorf     bool
+	sprintf1   bool
+	fiximports bool
 }
 
 func newPerfSprint() *perfSprint {
 	return &perfSprint{
-		intConv:  true,
-		errError: false,
-		errorf:   true,
-		sprintf1: true,
+		intConv:    true,
+		errError:   false,
+		errorf:     true,
+		sprintf1:   true,
+		fiximports: true,
 	}
 }
 
@@ -44,6 +46,7 @@ func New() *analysis.Analyzer {
 	r.Flags.BoolVar(&n.errError, "err-error", false, "optimizes into err.Error() even if it is only equivalent for non-nil errors")
 	r.Flags.BoolVar(&n.errorf, "errorf", true, "optimizes fmt.Errorf")
 	r.Flags.BoolVar(&n.sprintf1, "sprintf1", true, "optimizes fmt.Sprintf with only one argument")
+	r.Flags.BoolVar(&n.fiximports, "fiximports", true, "fix needed imports from other fixes")
 	return r
 }
 
@@ -486,7 +489,7 @@ func (n *perfSprint) run(pass *analysis.Pass) (interface{}, error) {
 		}
 	})
 
-	if len(removedFmtUsages) > 0 {
+	if len(removedFmtUsages) > 0 && n.fiximports {
 		for _, pkg := range pass.Pkg.Imports() {
 			if pkg.Path() == "fmt" {
 				insp = pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
